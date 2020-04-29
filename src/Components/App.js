e5ca1f2f6e090de9155b0e9bf6234002b3768ad9
abhay803 <abhayksingh3@gmail.com>
2020-04-29 14:13:06 +0530

    Added style in components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="dummy">
       <h1>Hi There !!!</h1>
     </div>
   );
